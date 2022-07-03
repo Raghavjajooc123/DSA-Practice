359048a2034b96f439d8911dc41208478fdb45de
Raghav Jajoo <61447812+Raghavjajooc123@users.noreply.github.com>
2022-07-03 22:49:49 +0530

    Create BreadthFirstSearch.cpp

---
+++ Binary Trees/BreadthFirstSearch.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+    node(int d){
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+node* buildTree(){
+    int d;
+    cin>>d;
+    if(d==-1){
+        return NULL;
+    }
+    node* root = new node(d);
+    root->left = buildTree();
+    root->right = buildTree();
+    return root;
+}
+
+void printTree(node* root){
+    if(root == NULL){
+        return;
+    }
+    cout<<root->data<<" ";
+    printTree(root->left);
+    printTree(root->right);
+    return;
+}
+
+void BreadthFirstSearch(node* root){
+    if(root == NULL){
+        return;
+    }
+    queue<node*> Q;
+    Q.push(root);
+    while(!Q.empty()){
+        node* f = Q.front();
+        cout<<f->data<<" ";
+        Q.pop();
+        if(f->left){
+            Q.push(f->left);
+        }
+        if(f->right){
+            Q.push(f->right);
+        }
+    }
+    return;
+}
+
+int main() {
+	// Breadth First search
+	node* root = buildTree();
+// 	printTree(root);
+// 	cout<<endl;
+	BreadthFirstSearch(root);
+	cout<<endl;
+	return 0;
+}
