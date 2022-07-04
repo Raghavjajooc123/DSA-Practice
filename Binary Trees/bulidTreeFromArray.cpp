a9f864c9111620ef2d234084494e39e8baa490c2
Raghav Jajoo <61447812+Raghavjajooc123@users.noreply.github.com>
2022-07-04 16:12:04 +0530

    Create bulidTreeFromArray.cpp

---
+++ Binary Trees/bulidTreeFromArray.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+class node{
+    public:
+        int data;
+        node* left;
+        node* right;
+    node(int d){
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+node* bulidTreeFromArray(int* arr, int s, int e){
+    if(s>e){
+        return NULL;
+    }
+    int mid = (s+e)/2;
+    node* root = new node(arr[mid]);
+    root->left = bulidTreeFromArray(arr, s, mid-1);
+    root->right = bulidTreeFromArray(arr, mid+1, e);
+    return root;
+}
+
+void bfs(node* root){
+    if(root == NULL){
+        return;
+    }
+    queue<node*> q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        node* f = q.front();
+        if(f==NULL){
+            q.pop();
+            cout<<endl;
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+        else{
+            cout<<f->data<<" ";
+            q.pop();
+            if(f->left)
+                q.push(f->left);
+            if(f->right)
+                q.push(f->right);
+        }
+    }
+    return;
+}
+
+int main() {
+	//
+	int n;
+	cin>>n;
+	int arr[1000];
+	for(int i=0; i<n; i++){
+	    cin>>arr[i];
+	}
+	node* root = bulidTreeFromArray(arr, 0, n-1);
+	bfs(root);
+	return 0;
+}
