07feb1573ba334f5861cbc3fd4fd75b9e582e2fc
Raghav Jajoo <61447812+Raghavjajooc123@users.noreply.github.com>
2022-07-05 12:38:42 +0530

    Create Q1_TwoSum.cpp

---
+++ LeetCode/Q1_TwoSum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i, j;
+        
+        for(i=0; i<nums.size(); i++){
+            for(j=i+1; j<nums.size(); j++){
+                if(nums[i]+nums[j] == target){
+                    vector<int> ans = {i, j};
+                    return ans;
+                }
+            }
+        }
+        vector<int> ans = {i, j};
+        return ans;
+    }
+};
