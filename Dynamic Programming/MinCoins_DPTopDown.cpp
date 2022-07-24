bd01bca5c5debea364ba3ccb24609b15fd6a508c
Raghav Jajoo <61447812+Raghavjajooc123@users.noreply.github.com>
2022-07-25 03:13:59 +0530

    Update MinCoins_DPTopDown.cpp

--- Dynamic Programming/MinCoins_DPTopDown.cpp
+++ Dynamic Programming/MinCoins_DPTopDown.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int min_coins(int n, int currencySize, int* coins, int * minCoinsStorage){
+    //base case
+    if(n == 0){
+        return 0;
+    }
+    //Stored value
+    if(minCoinsStorage[n] != 0){
+        return minCoinsStorage[n];
+    }
+    //rec case
+    int minimumCoins = INT_MAX;
+    for(int i=0; i<currencySize && coins[i]<=n; i++){
+        int op = min_coins(n - coins[i], currencySize, coins, minCoinsStorage);
+        minimumCoins = min(minimumCoins, op);
+    }
+    minCoinsStorage[n] = minimumCoins + 1;
+    return minCoinsStorage[n];
+}
 
+int main() {
+	// minimum steps to one
+	int n; 
+	cin>>n;
+	int coins[] = {1, 7, 10};
+	int currencySize = 3;
+	int minCoinsStorage[1000] = {0};
+	for(int i=0; i<currencySize; i++){
+	    minCoinsStorage[coins[i]] = 1;
+	}
+	cout<<min_coins(n, currencySize, coins, minCoinsStorage)<<endl;
+	return 0;
+}
