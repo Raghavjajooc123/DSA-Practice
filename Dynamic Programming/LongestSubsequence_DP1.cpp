91551633676b588524b4cc4f02b4b0efb692642c
Raghav Jajoo <61447812+Raghavjajooc123@users.noreply.github.com>
2022-07-26 02:32:55 +0530

    Create LongestSubsequence_DP1.cpp

---
+++ Dynamic Programming/LongestSubsequence_DP1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+int LongestSubsequence_DP1(int * arr, int n){
+    int dp[100] = {1};
+    for(int i=1; i<n; i++){
+        int maxlen = 0;
+        for(int j=1; j<i; j++){
+            if(arr[j]<arr[i]){
+                maxlen = max(maxlen, dp[j]);
+            }
+        }
+        dp[i] = maxlen + 1;
+    }
+    //printing the array
+    for(int i=0; i<n; i++){
+        cout<<dp[i]<<" ";
+    }
+    cout<<endl;
+    int maxlen = 0;
+    for(int i=0; i<n; i++){
+        maxlen = max(maxlen, dp[i]);
+    }
+    return maxlen+1;
+}
+int main() {
+	// Longest subsequences DP-1
+	int n;
+	cin>>n;
+	int arr[100];
+	for(int i=0; i<n; i++){
+	    cin>>arr[i];
+	}
+	cout<<LongestSubsequence_DP1(arr, n)<<endl;
+	return 0;
+}
