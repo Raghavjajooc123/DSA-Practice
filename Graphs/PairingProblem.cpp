87473c34ca4624f3c24e0b0972fa28865f5e53fa
Raghav Jajoo <61447812+Raghavjajooc123@users.noreply.github.com>
2022-08-03 19:38:31 +0530

    Create PairingProblem.cpp

---
+++ Graphs/PairingProblem.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <list>
+using namespace std;
+class Graph{
+    int V;
+    //edge list representation {(2,3), (0,1).......}
+    list<pair<int, int>> l;
+    public:
+    Graph(int v){
+        this->V = v;
+    }
+    void addEdge(int a, int b){
+        l.push_back(make_pair(a,b));
+    }
+    //find function
+    int findSet(int i, int parent[]){
+        if(parent[i] == -1){
+            return i;
+        }
+        //optimization 1
+        return parent[i] = findSet(parent[i], parent);
+    }
+    //union set
+    void unionSet(int x, int y, int parent[], int rank[]){
+        int s1 = findSet(x, parent);
+        int s2 = findSet(y, parent);
+        //optimization-2 ----- union by rank
+        if(s1!=s2){
+            if(rank[s1]<rank[s2]){
+                parent[s1] = s2;
+                rank[s2] = rank[s1]+rank[s2];
+            }
+            else{
+                parent[s2] = s1;
+                rank[s1] = rank[s1]+rank[s2];
+            }
+        }
+        return;
+    }
+    int Pairing(){
+        int * parent = new int[V];
+        int * rank = new int[V];
+        for(int i=0; i<V; i++){
+            parent[i] = -1;
+            rank[i] = 1;
+        }
+        for(auto edge:l){
+            int i = edge.first;
+            int j = edge.second;
+            int s1 = findSet(i, parent);
+            int s2 = findSet(j, parent);
+            unionSet(s1, s2, parent, rank);
+        }
+        int ans = 0;
+        for(int i=0; i<V; i++){
+            ans += V - rank[findSet(i, parent)];
+        }
+        delete [] parent;
+        delete [] rank;
+        return ans/2;
+    }
+};
+
+int main() {
+	// Pairing problem using DSU
+	int n, m;
+	cin>>n>>m;
+	
+	Graph g(n);
+	
+	while(m--){
+	    int x,y;
+	    cin>>x>>y;
+	    g.addEdge(x,y);
+	    
+	}
+	
+	cout<<g.Pairing()<<endl;
+	return 0;
+}
