c539f51fffa247b5d88c9e261edb521872f6fbb4
Raghav Jajoo <61447812+Raghavjajooc123@users.noreply.github.com>
2022-08-07 19:47:39 +0530

    Create Q18_ReversePair____Brute.cpp

---
+++ StriverSheet/Day3/Q18_ReversePair____Brute.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+    int reversePairs(vector<int>& nums) {
+        
+        //brute force
+        // int ans = 0;
+        // int n = nums.size();
+        // for(int i=0; i<n; i++){
+        //     for(int j=i+1; j<n; j++){
+        //         double temp = nums[j];
+        //         temp+=nums[j];
+        //         if(nums[i] > temp){
+        //             ans++;
+        //         }
+        //     } 
+        // }
+        // return ans;
+    }
+};
